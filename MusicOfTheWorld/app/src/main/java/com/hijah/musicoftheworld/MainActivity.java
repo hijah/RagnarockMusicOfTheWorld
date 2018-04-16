dc6981a118c8914a95908fc6dd1df0aabfa758fd
hijah <jacob.aa@hotmail.com>
2018-04-16 15:10:38 +0200

    popup men med fejl

--- MusicOfTheWorld/app/src/main/java/com/hijah/musicoftheworld/MainActivity.java
+++ MusicOfTheWorld/app/src/main/java/com/hijah/musicoftheworld/MainActivity.java
@@ -1,13 +1,53 @@
 package com.hijah.musicoftheworld;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Close;
+    Button Create;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Create = (Button) findViewById(R.id.buttonEastAsia);
+        Context mContext = getApplicationContext();
     }
+
+    public void Popup(View view) {
+        showPopup();
+    }
+
+
+
+    private PopupWindow pw;
+    private void showPopup() {
+        try {
+// We need to get the instance of the LayoutInflater
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.popup,
+                    (ViewGroup) findViewById(R.id.popup_1));
+            pw = new PopupWindow(layout, 300, 370, true);
+            pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
+            Close = (Button) layout.findViewById(R.id.close_popup);
+            Close.setOnClickListener(cancel_button);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private View.OnClickListener cancel_button = new View.OnClickListener() {
+        public void onClick(View v) {
+            pw.dismiss();
+        }
+    };
 }
