66edc124e8f17d781f1429b34372bb74564225c0
hijah <jacob.aa@hotmail.com>
2018-04-16 16:35:15 +0200

    popup virker

--- MusicOfTheWorld/app/src/main/java/com/hijah/musicoftheworld/MainActivity.java
+++ MusicOfTheWorld/app/src/main/java/com/hijah/musicoftheworld/MainActivity.java
@@ -1,53 +1,48 @@
 package com.hijah.musicoftheworld;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.PopupWindow;
+import android.view.ViewGroup.LayoutParams;
 
-public class MainActivity extends AppCompatActivity {
+import com.hijah.musicoftheworld.R;
 
-    Button Close;
-    Button Create;
-    private Context mContext;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnOpenPopup;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Create = (Button) findViewById(R.id.buttonEastAsia);
-        Context mContext = getApplicationContext();
+
+        btnOpenPopup = (Button) findViewById(R.id.buttonEastAsia);
+        btnOpenPopup.setOnClickListener(this);
     }
 
-    public void Popup(View view) {
-        showPopup();
+    @Override
+    public void onClick(View view) {
+        LayoutInflater layoutInflater
+                = (LayoutInflater) getBaseContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = layoutInflater.inflate(R.layout.popup, null);
+        final PopupWindow popupWindow = new PopupWindow(
+                popupView,
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+
+        Button btnDismiss = (Button) popupView.findViewById(R.id.dismiss);
+        btnDismiss.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+
+        popupWindow.showAsDropDown(btnOpenPopup, 20, 90);
     }
-
-
-
-    private PopupWindow pw;
-    private void showPopup() {
-        try {
-// We need to get the instance of the LayoutInflater
-            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
-            View layout = inflater.inflate(R.layout.popup,
-                    (ViewGroup) findViewById(R.id.popup_1));
-            pw = new PopupWindow(layout, 300, 370, true);
-            pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
-            Close = (Button) layout.findViewById(R.id.close_popup);
-            Close.setOnClickListener(cancel_button);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    private View.OnClickListener cancel_button = new View.OnClickListener() {
-        public void onClick(View v) {
-            pw.dismiss();
-        }
-    };
 }
